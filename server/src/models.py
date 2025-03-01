06aba7f7c55cf2b13953eacb728ab1586102d7cd
Priyankesh <priyankeshom@gmail.com>
2025-03-02 00:13:05 +0530

    Add Alembic configuration and initial migration setup

--- server/src/models.py
+++ server/src/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Enum
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 import enum
 
